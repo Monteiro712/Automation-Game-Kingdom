d3e32522dc8dde0a15b48cf99026c5df7ab53611
Jonathan <jonathanmonteiroa@gmail.com>
2023-11-18 22:39:30 -0300

    code improve with functions

--- automation.py
+++ automation.py
@@ -1,32 +1,38 @@
-from selenium import webdriver #simulate brawser use
-from selenium.webdriver.common.by import By #find informations 
-import openpyxl #create workboot
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+import openpyxl 
+
+def collect_data(url):
+    driver.get(url)
+    titles = driver.find_elements(By.XPATH, "//h3[@class='product-name']")
+    prices = driver.find_elements(By.XPATH, "//span[@class='price-boleto']")
+    return zip(titles, prices)
+
+def add_to_sheet(data, sheet):
+    for title, price in data:
+        sheet.append([title.text, price.text])
 
 driver = webdriver.Chrome()
 
 workboot = openpyxl.Workbook()
-workboot.create_sheet('Games')
+workboot.create_sheet('Games PS4')
 
-sheet_games = workboot['Games']
-sheet_games['A1'].value = 'Game'
-sheet_games['B1'].value = 'Price'
+sheet_games_ps4 = workboot['Games PS4']
+sheet_games_ps4['A1'].value = 'Game'
+sheet_games_ps4['B1'].value = 'Price'
 
-# Primeira URL
-driver.get('https://www.lojasgamemania.com.br/jogo-playstation-4')
+data = collect_data('https://www.lojasgamemania.com.br/jogo-playstation-4')
+add_to_sheet(data, sheet_games_ps4)
 
-titles = driver.find_elements(By.XPATH, "//h3[@class='product-name']")
-prices = driver.find_elements(By.XPATH, "//span[@class='price-boleto']")
+data = collect_data('https://www.lojasgamemania.com.br/jogo-playstation-4?p=2')
+add_to_sheet(data, sheet_games_ps4)
 
-for title, price in zip(titles, prices):
-    sheet_games.append([title.text, price.text])
+workboot.create_sheet('Games SWITCH')
+sheet_games_sw = workboot['Games SWITCH']
+sheet_games_sw['A1'].value = 'Game'
+sheet_games_sw['B1'].value = 'Price'
 
-# Segunda URL
-driver.get('https://www.lojasgamemania.com.br/jogo-playstation-4?p=2')
-
-titles = driver.find_elements(By.XPATH, "//h3[@class='product-name']")
-prices = driver.find_elements(By.XPATH, "//span[@class='price-boleto']")
-
-for title, price in zip(titles, prices):
-    sheet_games.append([title.text, price.text])
+data = collect_data('https://www.lojasgamemania.com.br/jogos-de-nintendo-switch')
+add_to_sheet(data, sheet_games_sw)
 
 workboot.save('Games.xlsx')
